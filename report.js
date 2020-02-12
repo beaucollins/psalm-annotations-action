a93ba85a42807d4bd3283c32f4621be5bf200b79
Beau Collins <beau@collins.pub>
2020-02-12 15:52:14 -0800

    try to use the action

---
+++ report.js
@@ -0,0 +1,17 @@
+const core = require( '@actions/core' );
+const github = require( '@actions/github' );
+
+try {
+    const path = core.getInput('report_path');
+    core.setFailed('Not implemented');
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+/**
+ * 
+ * @param {string} path 
+ */
+function readContents( path ) {
+    throw new Error('not implemented');
+}
